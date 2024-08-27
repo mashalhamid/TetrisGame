1774fde4aa9f04570fdf960e2c6837b74ba7af44
Mashal Hamid <mashalhamid2@gmail.com>
2024-08-27 22:28:24 +1000

    changed position of gamearea

--- src/tetris/GameArea.java
+++ src/tetris/GameArea.java
@@ -19,7 +19,7 @@ public class GameArea extends JPanel {
 
     public GameArea(int columns) //constructor
     {
-        setBounds(50, 100, 200, 400); // Set bounds directly in the GameArea constructor
+        setBounds(130, 90, 200, 400);
         setBorder(BorderFactory.createLineBorder(Color.lightGray, 2));
 
         gridColumns = columns;
